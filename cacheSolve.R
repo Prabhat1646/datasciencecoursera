faa5af7aacf0e997fc80f15d842c3954fd68eb17
Prabhat1646 <67097387+Prabhat1646@users.noreply.github.com>
2020-06-28 19:52:42 +0530

    Add files via upload

---
+++ cacheSolve.R
@@ -0,0 +1,13 @@
+cacheSolve <- function(x, ...) {
+  i <- x$getInverse()
+  if(!is.null(i)){
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data)
+  x$setInverse(i)
+  i      
+}
+
+
